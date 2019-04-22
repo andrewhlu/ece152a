626b14f76f002df70da564a93c2ffa2a601f96a8
andrewhlu <alu@ucsb.edu>
2019-04-21 18:25:27 -0700

    Change full adder to not include XOR gates

--- lab2/fullAdder.v
+++ lab2/fullAdder.v
@@ -7,15 +7,18 @@ module fullAdder (
 );
 
 wire AxB;
-assign AxB = A ^ B;
+// assign AxB = A ^ B;
+assign AxB = (A & ~B) | (~A & B);
 
 wire AxBaCin;
-assign AxBaCin = (A ^ B) & Cin;
+// assign AxBaCin = (A ^ B) & Cin;
+assign AxBaCin = ((A & ~B) | (~A & B)) & Cin;
 
 wire AaB;
 assign AaB = A & B;
 
 assign Cout = AaB | AxBaCin;
-assign S = AxB ^ Cin;
+// assign S = AxB ^ Cin;
+assign S = (AxB & ~Cin) | (~AxB & Cin);
 
 endmodule
